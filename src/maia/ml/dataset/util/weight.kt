2245199cdfc1131ebe8afbbf2af93a6a95f23463
samst <samsteed4@gmail.com>
2022-02-02 09:25:49 +1300

    Changed the initialisation of weight

--- src/maia/ml/dataset/util/weight.kt
+++ src/maia/ml/dataset/util/weight.kt
@@ -11,5 +11,6 @@ import maia.ml.dataset.WithWeight
  * Gets the weight of a data-row, defaulting to 1.0 if the
  * row is unweighted.
  */
-val DataRow.weight: Double
+var DataRow.weight: Double
     get() = if (this is WithWeight) weight else 1.0
+    set(value) {}
