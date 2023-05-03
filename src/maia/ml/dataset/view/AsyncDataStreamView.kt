1ccc6966fa02ae5ea5fe19fcdf8b042dc8540ec6
csterling <coreytsterling@gmail.com>
2023-05-03 15:32:50 +1200

    Added async variant of DataStreamView.

---
+++ src/maia/ml/dataset/view/AsyncDataStreamView.kt
@@ -0,0 +1,63 @@
+package maia.ml.dataset.view
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import maia.ml.dataset.AsyncDataStream
+import maia.util.datastructure.OrderedHashSet
+import maia.util.datastructure.OrderedSet
+import maia.ml.dataset.DataMetadata
+import maia.ml.dataset.DataRow
+import maia.ml.dataset.headers.DataColumnHeadersView
+import maia.ml.dataset.mutable.MutableDataRow
+
+/**
+ * A read-only view of a sub-set of columns from a data-stream.
+ *
+ * @param source    The data-stream to source rows from.
+ * @param columns   The sub-set of columns to view, or null for all columns.
+ */
+open class AsyncDataStreamView(
+    source : AsyncDataStream<*>,
+    protected val columns : OrderedSet<Int>? = null
+) : AsyncDataStream<DataRowView> {
+
+    constructor(
+        source : AsyncDataStream<*>,
+        columns : Iterator<Int>
+    ) : this(
+        source,
+        OrderedHashSet<Int>().also { set ->
+            columns.forEach { set.add(it) }
+        } as OrderedSet<Int>?
+    )
+
+    constructor(source : AsyncDataStream<*>, vararg columns : Int) : this(source, columns.iterator())
+
+    protected open val source = source
+
+    override val metadata: DataMetadata
+        get() = source.metadata
+
+    override val headers = DataColumnHeadersView(source.headers, columns)
+
+    override val numColumns: Int = headers.size
+
+    override fun rowFlow() : Flow<DataRowView> {
+        return source.rowFlow().map(this::wrapRow)
+    }
+
+    protected fun wrapRow(sourceRow : DataRow) : DataRowView {
+        return if (sourceRow is MutableDataRow)
+            MutableDataRowView(sourceRow, columns)
+        else
+            DataRowView(sourceRow, columns)
+    }
+}
+
+fun AsyncDataStream<*>.readOnlyView() : AsyncDataStreamView {
+    return AsyncDataStreamView(this)
+}
+
+fun AsyncDataStream<*>.readOnlyViewColumns(columns : OrderedSet<Int>) : AsyncDataStreamView {
+    return AsyncDataStreamView(this, columns)
+}
--- src/maia/ml/dataset/view/DataStreamView.kt
+++ src/maia/ml/dataset/view/DataStreamView.kt
@@ -1,13 +1,10 @@
 package maia.ml.dataset.view
 
+import kotlinx.coroutines.flow.Flow
 import maia.util.datastructure.OrderedHashSet
 import maia.util.datastructure.OrderedSet
 import maia.util.map
-import maia.ml.dataset.DataMetadata
-import maia.ml.dataset.DataRow
 import maia.ml.dataset.DataStream
-import maia.ml.dataset.headers.DataColumnHeadersView
-import maia.ml.dataset.mutable.MutableDataRow
 
 /**
  * A read-only view of a sub-set of columns from a data-stream.
@@ -17,8 +14,8 @@ import maia.ml.dataset.mutable.MutableDataRow
  */
 open class DataStreamView(
     source : DataStream<*>,
-    protected val columns : OrderedSet<Int>? = null
-) : DataStream<DataRowView> {
+    columns : OrderedSet<Int>? = null
+) : AsyncDataStreamView(source, columns), DataStream<DataRowView> {
 
     constructor(
         source : DataStream<*>,
@@ -32,24 +29,14 @@ open class DataStreamView(
 
     constructor(source : DataStream<*>, vararg columns : Int) : this(source, columns.iterator())
 
-    protected open val source = source
-
-    override val metadata: DataMetadata
-        get() = source.metadata
-
-    override val headers = DataColumnHeadersView(source.headers, columns)
-
-    override val numColumns: Int = headers.size
+    override val source = source
 
     override fun rowIterator(): Iterator<DataRowView> {
         return source.rowIterator().map(this::wrapRow)
     }
 
-    protected fun wrapRow(sourceRow : DataRow) : DataRowView {
-        return if (sourceRow is MutableDataRow)
-            MutableDataRowView(sourceRow, columns)
-        else
-            DataRowView(sourceRow, columns)
+    override fun rowFlow() : Flow<DataRowView> {
+        return super<AsyncDataStreamView>.rowFlow()
     }
 }
 
