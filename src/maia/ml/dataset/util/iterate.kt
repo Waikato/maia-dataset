34ced0ac952195a506cd1dbf0f5df4f96b1e7c7c
csterling <coreytsterling@gmail.com>
2023-05-17 10:39:40 +1200

    Abstracted forEachColumn to all columned structures.

--- src/maia/ml/dataset/util/iterate.kt
+++ src/maia/ml/dataset/util/iterate.kt
@@ -1,19 +1,19 @@
 package maia.ml.dataset.util
 
-import maia.ml.dataset.DataRow
+import maia.ml.dataset.WithColumns
 import maia.ml.dataset.headers.header.DataColumnHeader
 
 /**
- * Iterates over all columns of a data-row.
+ * Iterates over all columns of a data-structure.
  *
  * @receiver
- *          The data-row to iterate over.
+ *          The data-structure to iterate over.
  * @param block
  *          The action to perform on each column.
  */
-inline fun DataRow.forEachColumn(
+inline fun WithColumns.forEachColumn(
     start: Int = 0,
-    end: Int = headers.size,
+    end: Int = numColumns,
     block: (Int, DataColumnHeader) -> Unit
 ) {
     for (columnIndex in start until end) {
